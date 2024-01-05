37ebec5bfd6482bd5f10c0bd175ac4b87f6fdb25
Stephen Von Worley <svonworl@ucsc.edu>
2024-01-05 10:08:07 -0800

    complete update-down-page.sh

--- scripts/update-down-page.sh
+++ scripts/update-down-page.sh
@@ -24,6 +24,8 @@ if [ -z "$UPDATE_ENV" ] || [ ! -f "$INDEX_HTML_PATH" ]; then
   exit 1
 fi
 
+INDEX_HTML="$(cat $INDEX_HTML_PATH)"
+
 if [ $UPDATE_ENV == "dev" ]; then
   export BUCKET_DIR="develop"
   export AWS_DEFAULT_REGION="us-east-1"
@@ -37,35 +39,49 @@ elif [ $UPDATE_ENV == "prod" ]; then
   export BUCKET_DIR="production"
   export AWS_DEFAULT_REGION="us-east-1"
 else
-  echo "Invalid environment $UPDATE_ENV; must be dev, qa, staging or prod"
+  echo "Invalid environment $UPDATE_ENV; must be dev, qa, staging, or prod"
   print_usage
   exit 1
 fi
 
-INDEX_HTML="$(cat $INDEX_HTML_PATH)"
+
 WEBACL_NAME="WebACL-${UPDATE_ENV}-down"
+echo "Updating WebACL '${WEBACL_NAME}' using content from '${INDEX_HTML_PATH}'."
 
-echo "UPDATE_ENV $UPDATE_ENV"
-echo "BUCKET_NAME $BUCKET_NAME"
-echo "BUCKET_DIR $BUCKET_DIR"
-echo "INDEX_HTML_PATH $INDEX_HTML_PATH"
-echo "INDEX_HTML $INDEX_HTML"
-echo "WEBACL_NAME $WEBACL_NAME"
+# Retrieve a list of WebACLs and extract the summary by name.
+WEBACL_SUMMARY="$(aws wafv2 list-web-acls --scope REGIONAL | jq '.WebACLs' | jq "map(select(.Name == \"${WEBACL_NAME}\"))" | jq '.[0]')"
+if [[ "${WEBACL_SUMMARY}" == "null" ]]; then
+  echo "Could not find WebACL '${WEBACL_NAME}'."
+  echo "Exiting."
+  exit 1
+fi
 
-WEBACLS="$(aws wafv2 list-web-acls --scope REGIONAL)"
-WEBACL_SUMMARY="$(echo "$WEBACLS" | jq '.WebACLs' | jq "map(select(.Name == \"${WEBACL_NAME}\"))" | jq '.[0]')"
-WEBACL_ID="$(echo "$WEBACL_SUMMARY" | jq -r .Id)"
-WEBACL_NAME="$(echo "$WEBACL_SUMMARY" | jq -r .Name)"
-WEBACL_AND_LOCKTOKEN="$(aws wafv2 get-web-acl --scope REGIONAL --id "$WEBACL_ID" --name "$WEBACL_NAME")"
-echo "WEBACL_AND_LOCKTOKEN ${WEBACL_AND_LOCKTOKEN}"
-WEBACL="$(echo "$WEBACL_AND_LOCKTOKEN" | jq '.WebACL')"
-WEBACL_LOCKTOKEN="$(echo "$WEBACL_AND_LOCKTOKEN" | jq '.LockToken')"
-WEBACL_MODIFIED="$(echo "$WEBACL" | jq --rawfile html "${INDEX_HTML_PATH}" '.CustomResponseBodies["response"].Content = $html')"
-echo "WEBACL_MODIFIED ${WEBACL_MODIFIED}"
+# Extract the ID.
+WEBACL_ID="$(echo "${WEBACL_SUMMARY}" | jq -r .Id)"
+
+# Retrieve the full WebACL and associated lock token.
+WEBACL_AND_LOCKTOKEN="$(aws wafv2 get-web-acl --scope REGIONAL --id "${WEBACL_ID}" --name "${WEBACL_NAME}")"
+WEBACL="$(echo "${WEBACL_AND_LOCKTOKEN}" | jq '.WebACL')"
+WEBACL_LOCKTOKEN="$(echo "${WEBACL_AND_LOCKTOKEN}" | jq '.LockToken')"
+
+# Change the custom response body to the new content.
+WEBACL_MODIFIED="$(echo "${WEBACL}" | jq --rawfile html "${INDEX_HTML_PATH}" '.CustomResponseBodies["response"].Content = $html')"
+
+# Retrieve the AWS-suggested skeletion of the update json.
 WEBACL_SKELETON="$(aws wafv2 update-web-acl --generate-cli-skeleton)"
-WEBACL_UPDATE="$(echo "$WEBACL_MODIFIED" | jq '.Scope = "REGIONAL" | .LockToken = '"${WEBACL_LOCKTOKEN}"' | '"$(echo "${WEBACL_SKELETON}" | jq 'keys' | tr '[]' '{}')")"
-echo "WEBACL_UPDATE ${WEBACL_UPDATE}"
-aws wafv2 update-web-acl --cli-input-json "${WEBACL_UPDATE}"
-# zzz
 
-# add s3 copy
+# Add "Scope" and "LockToken" fields to the modified WebACL json, then remove any fields that don't appear in the skeleton.
+# We must remove fields that don't appear in the skeleton because AWS rejects updates that include them.
+WEBACL_UPDATE="$(echo "${WEBACL_MODIFIED}" | jq '.Scope = "REGIONAL" | .LockToken = '"${WEBACL_LOCKTOKEN}"' | '"$(echo "${WEBACL_SKELETON}" | jq 'keys' | tr '[]' '{}')")"
+
+# Update the WebACL.
+aws wafv2 update-web-acl --cli-input-json "${WEBACL_UPDATE}"
+echo "Updated WebACL."
+
+
+BUCKET_S3="s3://${BUCKET_NAME}/${BUCKET_DIR}/index.html"
+echo "Updating down content S3 object '${BUCKET_S3}' using content from '${INDEX_HTML_PATH}'."
+
+# Update the down content bucket.
+aws s3 cp "${INDEX_HTML_PATH}" "${BUCKET_S3}"
+echo "Updated bucket content."
